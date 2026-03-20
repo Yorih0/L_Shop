69bcb7580a4c94af4e138cf18c57f4dfce676157
Фнилер <leshag999@gmail.com>
2026-03-20 11:21:16 +0300

    shop

---
+++ client/src/pages/ShopPage.tsx
@@ -0,0 +1,56 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import ShopForm from "./ShopForm";
+import "./css/shop.css";
+
+// Просто импортируем каждое изображение
+import vector1 from "./img/main/Vector.png";
+import vector2 from "./img/main/Vector (1).png";
+import vector3 from "./img/main/Vector (2).png";
+import blend1 from "./img/main/Blend Group 1.png";
+import blend2 from "./img/main/Blend Group 2.png";
+import macbookPro from "./img/main/Macbook_16_Pro.png";
+import macbookAir from "./img/main/Macbook_14_Air.png";
+import iphone from "./img/main/Iphone_17_Pro_Max.png";
+import airpods from "./img/main/AirPods_3_Pro.png";
+
+export default function ShopPage() {
+  return (
+    <>
+      <Header />
+      <main className="main-s">
+        <div className="News">
+          <div className="hello_screen">
+            <h1 className="title">
+              Гармония <span className="gradient-text">совершенства</span>
+            </h1>
+            
+            <img src={vector1} className="vector1" alt="" />
+            <img src={vector2} className="vector2" alt="" />
+            <img src={vector3} className="vector3" alt="" />
+            <img src={blend1} className="vector4" alt="" />
+            <img src={blend2} className="vector5" alt="" />
+            
+            <p className="subtitle">Новинки</p>
+            <div className="news">
+              <div className="new">
+                <img src={macbookPro} alt="" />
+              </div>
+              <div className="new">
+                <img src={macbookAir} alt="" />
+              </div>
+              <div className="new">
+                <img src={iphone} alt="" />
+              </div>
+              <div className="new">
+                <img src={airpods} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <ShopForm />
+      </main>
+      <Footer />
+    </>
+  );
+}
